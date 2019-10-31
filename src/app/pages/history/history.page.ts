add150656d4e7c22154fadaa69883dcc613f22ff
Cindy Michelle <cindy@kodefox.com>
2019-10-31 23:28:47 +0700

    add history page

--- src/app/pages/history/history.page.ts
+++ src/app/pages/history/history.page.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+type SegmentedValue = 'parking' | 'places';
 @Component({
   selector: 'app-history',
   templateUrl: './history.page.html',
   styleUrls: ['./history.page.scss'],
 })
 export class HistoryPage implements OnInit {
+  segmentedValue: SegmentedValue;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.segmentedValue = 'parking';
   }
 
+  ngOnInit() {}
 }
