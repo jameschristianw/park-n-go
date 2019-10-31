2015e6cac55c74b874e82a215fdc48283cf07d1d
Cindy Michelle <cindy@kodefox.com>
2019-10-31 21:04:10 +0700

    add profile page and router for auth, login. register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'auth', pathMatch: 'full' },
   {
     path: 'home',
     loadChildren: () =>
@@ -10,10 +10,20 @@ const routes: Routes = [
   },
   {
     path: 'register',
-    loadChildren: './pages/register/register.module#RegisterPageModule',
+    loadChildren: './pages/auth/register/register.module#RegisterPageModule',
   },
-  { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
-  { path: 'main', loadChildren: './pages/auth/main/main.module#MainPageModule' },
+  {
+    path: 'login',
+    loadChildren: './pages/auth/login/login.module#LoginPageModule',
+  },
+  {
+    path: 'auth',
+    loadChildren: './pages/auth/main/main.module#MainPageModule',
+  },
+  {
+    path: 'profile',
+    loadChildren: './pages/profile/profile.module#ProfilePageModule',
+  },
 ];
 
 @NgModule({
