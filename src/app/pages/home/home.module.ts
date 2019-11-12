8062bd63861618631dcec9bb86f6cff4830ac00c
James Christian Wira <jamesch888@gmail.com>
2019-11-12 17:20:16 +0700

    Home Map WIP 12/11

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { HomePage } from './home.page';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 @NgModule({
   imports: [
@@ -18,6 +19,7 @@ import { HomePage } from './home.page';
       }
     ])
   ],
-  declarations: [HomePage]
+  declarations: [HomePage],
+  providers: [Geolocation]
 })
 export class HomePageModule {}
