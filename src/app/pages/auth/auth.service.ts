4874458acf31d3cbfd14a3b8c909385d8f6254ab
Cindy Michelle <46435998+cindmichelle@users.noreply.github.com>
2019-11-13 13:38:22 +0700

    Init firestore, firebase Auth, connect backend register, and add new user (#13)
    * init firestore, firebase Auth, connect backend register, and add new user firestore
    
    * minor fixes

--- src/app/pages/auth/auth.service.ts
+++ src/app/pages/auth/auth.service.ts
@@ -6,7 +6,6 @@ import { Injectable } from '@angular/core';
 
 import { HttpClient } from '@angular/common/http';
 import { environment } from './../../../environments/environment';
-
 import { User } from 'src/app/model/user.model';
 interface AuthResponse {
   kind: string;
