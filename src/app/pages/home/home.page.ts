c82953c3cb55dc307dc654b93da188ce28eaf036
James Christian Wira <jamesch888@gmail.com>
2019-11-19 06:31:53 +0700

    Init Google Maps

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -51,8 +51,8 @@ export class HomePage implements OnInit {
     await this.platform.ready();
     await this.loadMap();
   }
-  loadMap() {
 
+  loadMap() {
     // This code is necessary for browser
     Environment.setEnv({
       API_KEY_FOR_BROWSER_RELEASE: 'AIzaSyAs-bPFk39cMX-gV34ksx3MrLXpcviS1NQ',
@@ -83,6 +83,10 @@ export class HomePage implements OnInit {
     });
     marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
       alert('clicked');
+    });
+
+    this.map.on(GoogleMapsEvent.CAMERA_MOVE).subscribe( loc => {
+      console.log(loc[0].target.lat, loc[0].target.lng);
     });
   }
 }
